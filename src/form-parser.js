f1a07162f6802e86055626c32264281d2a4f3424
Karl Stein <karl.stein.pro@gmail.com>
2020-02-18 11:30:22 -1000

    Updates dependencies Renames main file and its test file

--- src/form-parser.js
+++ src/form-parser.js
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2019 Karl STEIN
+ * Copyright (c) 2020 Karl STEIN
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
