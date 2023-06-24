9a84b85fc84f7a1917189d454d4f509862accfcc
Karl Stein <karl.stein.pro@gmail.com>
2023-06-23 19:58:06 -1000

    Migrated code to TypeScript

---
+++ src/index.ts
@@ -0,0 +1,541 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2023 Karl STEIN
+ */
+
+/**
+ * Builds an object from a string (ex: [colors][0][code]).
+ * @param str
+ * @param value
+ * @param context
+ */
+export function buildObject (str: string, value: any, context?: Record<string, any>): Record<string, any> {
+  if (str == null || str.length === 0) {
+    return value
+  }
+
+  let ctx = context
+
+  // Check missing brackets
+  if (typeof ctx === 'undefined' || ctx === null) {
+    const opening = (str.match(/\[/g) || []).length
+    const closing = (str.match(/]/g) || []).length
+
+    if (opening !== closing) {
+      if (opening > closing) {
+        throw new SyntaxError(`Missing closing ] in ${str}`)
+      } else {
+        throw new SyntaxError(`Missing opening [ in ${str}`)
+      }
+    }
+  }
+
+  const index = str.indexOf('[')
+
+  if (index === -1) {
+    // Field without brackets and without attribute
+    // ex: customField
+    ctx = buildObject(`[${str}]`, value, ctx)
+  } else if (index > 0) {
+    // Field without brackets and with attribute
+    // ex: customField[attr]
+    const rootField = str.substring(0, index)
+    const subtree = str.substring(index)
+    ctx = buildObject(`[${rootField}]${subtree}`, value, ctx)
+  } else {
+    // Field with brackets
+    // ex: [customField]
+    const end = str.indexOf(']', index + 1)
+    const subtree = str.substring(end + 1)
+    let key = str.substring(index + 1, end)
+
+    // Object attribute
+    // ex: [customField1] or ["10"] or ['10']
+    if (key.length > 0 && (!/^\d+$/.test(key) || /^(["'])[^"']+\1$/.test(key))) {
+      // Remove quotes in key
+      key = key.replace(/^["']/, '').replace(/["']$/, '')
+
+      // Create empty object if context is not defined
+      if (typeof ctx === 'undefined' || ctx === null) {
+        ctx = {}
+      }
+      const result = buildObject(subtree, value, ctx[key])
+
+      if (typeof result !== 'undefined') {
+        // Put value in attribute
+        ctx[key] = result
+      } else {
+        // Remove attribute
+        delete ctx[key]
+      }
+    } else {
+      // Create empty array if context is not defined
+      if (typeof ctx === 'undefined' || ctx === null) {
+        ctx = []
+      }
+
+      if (key.length === 0) {
+        // Array with dynamic index
+        // ex: []
+        const result = buildObject(subtree, value)
+
+        if (typeof result !== 'undefined') {
+          // Put value at the end of the array
+          ctx.push(result)
+        }
+      } else if (/^[0-9]+$/.test(key)) {
+        // Array with static index
+        // ex: [0]
+        const result = buildObject(subtree, value, ctx[key])
+        const keyIndex = parseInt(key, 10)
+
+        if (typeof result !== 'undefined') {
+          // Put value at the specified index
+          ctx[keyIndex] = result
+        } else {
+          // Remove existing value
+          ctx.splice(keyIndex, 1)
+        }
+      }
+    }
+  }
+  return ctx
+}
+
+/**
+ * Returns all fields in a form with the same name.
+ * @param name
+ * @param form
+ */
+export function getFieldsByName (name: string, form: HTMLFormElement): Array<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement> {
+  const fields: Array<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement> = []
+  const { elements } = form
+
+  Object.values(elements).forEach((el) => {
+    if (el instanceof HTMLInputElement || el instanceof HTMLSelectElement || el instanceof HTMLTextAreaElement) {
+      if (el.name === name) {
+        fields.push(el)
+      }
+    }
+  })
+  return fields
+}
+
+/**
+ * Checks if the field is a button.
+ * @param element
+ */
+export function isButton (element: Element): boolean {
+  return element instanceof HTMLButtonElement ||
+    (element instanceof HTMLInputElement && ['button', 'reset', 'submit'].includes(element.type))
+}
+
+/**
+ * Checks if the field is checkable.
+ * @param element
+ */
+export function isCheckableField (element: Element): boolean {
+  return element instanceof HTMLInputElement && ['checkbox', 'radio'].includes(element.type)
+}
+
+/**
+ * Checks if field has multiple values (based on the "multiple" attribute or when field's name contains []).
+ * @param element
+ */
+export function isMultipleField (element: Element): boolean {
+  // Field has attribute "multiple".
+  if (element instanceof HTMLSelectElement && element.multiple) {
+    return true
+  }
+  // Field name contains an empty array (example: "numbers[]").
+  return (element instanceof HTMLInputElement ||
+      element instanceof HTMLSelectElement ||
+      element instanceof HTMLTextAreaElement) &&
+    element.name != null && /\[]$/.test(element.name)
+}
+
+/**
+ * Replaces empty string by null value.
+ * @param value
+ */
+export function nullify (value: any): any {
+  if (typeof value === 'string') {
+    return value === '' ? null : value
+  }
+
+  if (value instanceof Array) {
+    return value.map((v) => nullify(v))
+  }
+
+  if (typeof value === 'object' && value != null) {
+    const obj: Record<string, any> = {}
+
+    Object.keys(value).forEach((key) => {
+      obj[key] = nullify(value[key])
+    })
+    return obj
+  }
+  return value
+}
+
+/**
+ * Removes extra spaces.
+ * @param value
+ */
+export function trim (value: any): any {
+  if (typeof value === 'string' && value.length > 0) {
+    return value.trim()
+  }
+
+  if (value instanceof Array) {
+    return value.map((v) => trim(v))
+  }
+
+  if (typeof value === 'object' && value != null) {
+    const obj: Record<string, any> = {}
+
+    Object.keys(value).forEach((key) => {
+      obj[key] = trim(value[key])
+    })
+    return obj
+  }
+  return value
+}
+
+/**
+ * Returns a boolean string.
+ * @param value
+ */
+export function parseBoolean (value: string): boolean | null {
+  if (value == null) {
+    return null
+  }
+  const bool = value.trim()
+
+  if (/^(true|1|on|yes)$/i.test(bool)) {
+    return true
+  }
+  if (/^(false|0|off|no)$/i.test(bool)) {
+    return false
+  }
+  return null
+}
+
+/**
+ * Parses a number string.
+ * @param value
+ */
+export function parseNumber (value: string): number | null {
+  if (value == null) {
+    return null
+  }
+  const str = value.trim()
+    // Remove spaces
+    .replace(/ /g, '')
+
+  let number = null
+
+  if (/^[+-]?[0-9]*[.,][0-9]+$/.test(str)) {
+    // Replace comma with dot (for languages where number contain a comma instead of a dot)
+    number = parseFloat(str.replace(/,/g, '.'))
+  }
+  if (/^[+-]?[0-9]+$/.test(str)) {
+    number = parseInt(str, 10)
+  }
+  return Number.isNaN(number) ? null : number
+}
+
+export type ParsingType = 'auto' | 'boolean' | 'number'
+
+/**
+ * Parses a value based on type.
+ * @param value
+ * @param type
+ */
+export function parseValue (value?: string, type: ParsingType = 'auto'): string | number | boolean | null {
+  if (value == null) {
+    return null
+  }
+  if (type === 'boolean') {
+    return parseBoolean(value)
+  }
+  if (type === 'number') {
+    return parseNumber(value)
+  }
+  if (type === 'auto') {
+    const bool = parseBoolean(value)
+
+    if (typeof bool === 'boolean') {
+      return bool
+    }
+
+    const number = parseNumber(value)
+
+    if (typeof number === 'number') {
+      return number
+    }
+  }
+  return value
+}
+
+export type ParseFieldOptions = {
+  dynamicTyping?: boolean
+  nullify?: boolean
+  smartTyping?: boolean
+  trim?: boolean
+}
+
+/**
+ * Returns the parsed value of the field.
+ * @param element
+ * @param options
+ */
+export function parseField (element: Element, options?: ParseFieldOptions) {
+  // Check field.
+  if (!(element instanceof HTMLInputElement) &&
+    !(element instanceof HTMLSelectElement) &&
+    !(element instanceof HTMLTextAreaElement) &&
+    !(element instanceof HTMLButtonElement)) {
+    throw new TypeError('field is not an instance of HTMLInputElement or HTMLSelectElement or HTMLTextAreaElement')
+  }
+
+  // Set default options.
+  const opts: ParseFieldOptions = {
+    dynamicTyping: true,
+    nullify: true,
+    smartTyping: true,
+    trim: true,
+    ...options
+  }
+
+  const isCheckable = isCheckableField(element)
+
+  const { form } = element
+  let value: any = element.value
+
+  // Fetch value from special fields.
+  if (element instanceof HTMLInputElement) {
+    if (isCheckable) {
+      if (isMultipleField(element)) {
+        if (form) {
+          value = getFieldsByName(element.name, form)
+            .filter((el) => el instanceof HTMLInputElement && el.checked)
+            .map((el) => el.value)
+        } else {
+          value = [value]
+        }
+      } else {
+        value = element.checked ? value : null
+      }
+    }
+  }
+
+  if (element instanceof HTMLSelectElement) {
+    if (element.multiple) {
+      value = []
+
+      // Collect values of selected options
+      if (element.options instanceof HTMLCollection) {
+        for (let o = 0; o < element.options.length; o += 1) {
+          if (element.options[o].selected) {
+            value.push(element.options[o].value)
+          }
+        }
+      }
+    }
+  }
+
+  if (opts.dynamicTyping) {
+    // Parse value excepted for special fields
+    if ((!isCheckable || (element instanceof HTMLInputElement && element.checked)) &&
+      !['email', 'file', 'password', 'search', 'url'].includes(element.type) &&
+      !['textarea'].includes(element.localName)) {
+      const dataType = element.dataset.type
+
+      if (dataType) {
+        // Parse value based on "data-type" attribute.
+        if (dataType === 'auto') {
+          if (value instanceof Array) {
+            for (let k = 0; k < value.length; k += 1) {
+              value[k] = parseValue(value[k])
+            }
+          } else {
+            value = parseValue(value)
+          }
+        } else if (dataType === 'number') {
+          if (value instanceof Array) {
+            for (let k = 0; k < value.length; k += 1) {
+              value[k] = parseNumber(value[k])
+            }
+          } else {
+            value = parseNumber(value)
+          }
+        } else if (dataType === 'boolean') {
+          if (value instanceof Array) {
+            for (let k = 0; k < value.length; k += 1) {
+              value[k] = parseBoolean(value[k])
+            }
+          } else {
+            value = parseBoolean(value)
+          }
+        } else {
+          // eslint-disable-next-line no-console
+          console.warn(`unknown data-type "${dataType}" for field "${element.name}"`)
+        }
+      } else if (opts.smartTyping) {
+        // Parse value based on "type" attribute.
+        if (element instanceof HTMLInputElement) {
+          if (['number', 'range'].includes(element.type)) {
+            if (value instanceof Array) {
+              for (let k = 0; k < value.length; k += 1) {
+                value[k] = parseNumber(value[k])
+              }
+            } else {
+              value = parseNumber(value)
+            }
+          }
+        }
+      } else if (value instanceof Array) {
+        // Parse value using regular expression.
+        for (let k = 0; k < value.length; k += 1) {
+          value[k] = parseValue(value[k])
+        }
+      } else {
+        value = parseValue(value)
+      }
+    }
+  }
+
+  // Removes extra spaces but ignore password.
+  if (opts.trim && (!(element instanceof HTMLInputElement) || element.type !== 'password')) {
+    value = trim(value)
+  }
+  // Replaces empty string by null.
+  if (opts.nullify) {
+    value = nullify(value)
+  }
+  return value
+}
+
+export type ParseFormOptions = {
+  cleanFunction?: (value: string, field: Element) => void
+  dynamicTyping?: boolean
+  filterFunction?: (element: Element) => boolean
+  ignoreButtons?: boolean
+  ignoreDisabled?: boolean
+  ignoreEmpty?: boolean
+  ignoreUnchecked?: boolean
+  nullify?: boolean
+  smartTyping?: boolean
+  trim?: boolean
+}
+
+/**
+ * Returns form values as an object.
+ * @param form
+ * @param options
+ */
+export function parseForm (form: HTMLFormElement, options?: ParseFormOptions): Record<string, any> {
+  if (!(form instanceof HTMLFormElement)) {
+    throw new TypeError('form is not an instance of HTMLFormElement')
+  }
+
+  // Set default options.
+  const opts = {
+    cleanFunction: null,
+    dynamicTyping: true,
+    filterFunction: null,
+    ignoreButtons: true,
+    ignoreDisabled: true,
+    ignoreEmpty: false,
+    ignoreUnchecked: false,
+    nullify: true,
+    smartTyping: true,
+    trim: true,
+    ...options
+  }
+
+  const fields: Record<string, any> = {}
+  const { elements } = form
+
+  for (let i = 0; i < elements.length; i += 1) {
+    const field = elements[i]
+    const isCheckable = isCheckableField(field)
+    const isMultiple = isMultipleField(field)
+
+    // Ignore non-form element.
+    if (!(field instanceof HTMLInputElement) &&
+      !(field instanceof HTMLSelectElement) &&
+      !(field instanceof HTMLTextAreaElement) &&
+      !(field instanceof HTMLButtonElement)) {
+      continue
+    }
+    // Ignore element without a name.
+    if (field.name == null || field.name.length === 0) {
+      continue
+    }
+    // Ignore buttons.
+    if (opts.ignoreButtons && isButton(field)) {
+      continue
+    }
+    // Ignore disabled element.
+    if (opts.ignoreDisabled && field.disabled) {
+      continue
+    }
+    // Ignore unchecked element.
+    if (opts.ignoreUnchecked && (field instanceof HTMLInputElement && !field.checked)) {
+      continue
+    }
+    // Ignore element not matching the filter.
+    // todo move filter after value is set to allow filtering based on value
+    if (opts.filterFunction && opts.filterFunction(field) !== true) {
+      continue
+    }
+
+    // Parse field value.
+    let value = parseField(field, opts)
+
+    // Execute custom clean function on fields with type different of password.
+    if (opts.cleanFunction && field.type !== 'password') {
+      if (value instanceof Array) {
+        for (let k = 0; k < value.length; k += 1) {
+          if (typeof value[k] === 'string' && value[k].length) {
+            value[k] = opts.cleanFunction(value[k], field)
+          }
+        }
+      } else if (typeof value === 'string' && value.length) {
+        value = opts.cleanFunction(value, field)
+      }
+    }
+
+    // todo move filterFunction here
+
+    // Ignore empty value.
+    if (opts.ignoreEmpty && (value === '' || value === null || typeof value === 'undefined')) {
+      continue
+    }
+
+    let { name } = field
+
+    // Remove array from name if present (ex: items[] => items).
+    name = name.replace(/\[]$/g, '')
+
+    // Reconstruct array or object.
+    const arrayIndex = name.indexOf('[')
+
+    if (arrayIndex !== -1) {
+      const attr = name.substring(0, arrayIndex)
+      const path = name.substring(arrayIndex)
+      fields[attr] = buildObject(path, value, fields[attr])
+      continue
+    }
+
+    // Add field to the list.
+    if (isCheckable && !isMultiple && (field instanceof HTMLInputElement && !field.checked)) {
+      fields[name] = null
+    } else {
+      fields[name] = value
+    }
+  }
+  return fields
+}
---
+++ test/lib.ts
@@ -0,0 +1,136 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2023 Karl STEIN
+ */
+
+/**
+ * Creates a button
+ * @param attrs
+ */
+export function createButton (attrs): HTMLButtonElement {
+  return createElement('button', { ...attrs, type: 'button' }) as HTMLButtonElement
+}
+
+/**
+ * Creates a checkbox input
+ * @param attrs
+ */
+export function createCheckbox (attrs): HTMLInputElement {
+  return createElement('input', { ...attrs, type: 'checkbox' }) as HTMLInputElement
+}
+
+/**
+ * Creates a HTML element
+ * @param tagName
+ * @param attrs
+ */
+export function createElement (tagName: string, attrs): HTMLElement {
+  const element = document.createElement(tagName)
+  const keys = Object.keys(attrs)
+
+  for (let i = 0; i < keys.length; i += 1) {
+    const attr = keys[i]
+
+    if (typeof attrs[attr] !== 'undefined') {
+      if (attr === 'dataset' || attr === 'style') {
+        Object.entries(attrs[attr]).forEach(([k, v]) => {
+          element[attr][k] = v
+        })
+      } else {
+        element.setAttribute(attr, attrs[attr])
+      }
+    }
+  }
+  return element
+}
+
+/**
+ * Creates a file input
+ * @param attrs
+ */
+export function createFileInput (attrs): HTMLInputElement {
+  return createElement('input', { ...attrs, type: 'file' }) as HTMLInputElement
+}
+
+/**
+ * Creates a form element
+ */
+export function createForm (): HTMLFormElement {
+  return document.createElement('form')
+}
+
+/**
+ * Creates a hidden input
+ * @param attrs
+ */
+export function createHiddenInput (attrs): HTMLInputElement {
+  return createElement('input', { ...attrs, type: 'hidden' }) as HTMLInputElement
+}
+
+/**
+ * Creates a number input
+ * @param attrs
+ */
+export function createNumberInput (attrs): HTMLInputElement {
+  return createElement('input', { ...attrs, type: 'number' }) as HTMLInputElement
+}
+
+/**
+ * Creates a password input
+ * @param attrs
+ */
+export function createPasswordInput (attrs): HTMLInputElement {
+  return createElement('input', { ...attrs, type: 'password' }) as HTMLInputElement
+}
+
+/**
+ * Creates a radio input
+ * @param attrs
+ */
+export function createRadio (attrs): HTMLInputElement {
+  return createElement('input', { ...attrs, type: 'radio' }) as HTMLInputElement
+}
+
+/**
+ * Creates a select
+ * @param attrs
+ */
+export function createSelect (attrs): HTMLSelectElement {
+  const { options } = attrs || {}
+  const selectAttributes = { ...attrs }
+
+  if (typeof selectAttributes.options !== 'undefined') {
+    delete selectAttributes.options
+  }
+
+  const select = createElement('select', selectAttributes) as HTMLSelectElement
+
+  if (options instanceof Array) {
+    for (let i = 0; i < options.length; i += 1) {
+      const option = createElement('option', options[i])
+      select.appendChild(option)
+    }
+  }
+  return select
+}
+
+/**
+ * Creates a textarea
+ * @param attrs
+ */
+export function createTextarea (attrs): HTMLTextAreaElement {
+  const element = createElement('textarea', { ...attrs }) as HTMLTextAreaElement
+
+  if (typeof attrs.value !== 'undefined') {
+    element.appendChild(document.createTextNode(attrs.value))
+  }
+  return element
+}
+
+/**
+ * Creates a text input
+ * @param attrs
+ */
+export function createTextInput (attrs): HTMLInputElement {
+  return createElement('input', { type: 'text', ...attrs }) as HTMLInputElement
+}
